47416ff48263de06ad41285dee208feb3565434c
Toanhn1 <nguyenban61@gmail.com>
2018-08-29 10:40:40 +0700

    List order

--- web2py/applications/FristSample/controllers/displayhelloword.py
+++ web2py/applications/FristSample/controllers/displayhelloword.py
@@ -1,7 +1,21 @@
 #sample index page
+
 def index():
-    listOrder = get_list_order()
+    return dict()
+
+def allorder():
+    listOrder = db().select(db.orders.ALL)
     return dict(list = listOrder)
 
-def get_list_order():
-    return db().select(db.orders.ALL)
+def showdetail():
+    orderId = request.args[0]
+    order_detail = db(db.order_detail.user_id.like(orderId)).select()
+    return dict(list = order_detail)
+
+def deleteorder():
+    orderId = request.args[0]
+    db(db.orders.order_id == orderId).delete()
+    return dict(idDel = orderId)
+
+def showhome():
+    print("")
