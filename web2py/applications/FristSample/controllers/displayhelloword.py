8b7d228c88a2c1d40066f6cac2c6f8d833943a0d
Toanhn1 <nguyenban61@gmail.com>
2018-08-29 14:21:48 +0700

    Update more add.list del,view order

--- web2py/applications/FristSample/controllers/displayhelloword.py
+++ web2py/applications/FristSample/controllers/displayhelloword.py
@@ -7,6 +7,16 @@ def allorder():
     listOrder = db().select(db.orders.ALL)
     return dict(list = listOrder)
 
+def formadd():
+    return dict()
+
+def addorder():
+    db.orders.insert(order_id=request.vars.orderId, user_id=request.vars.userId, ngay_mua=request.vars.ngayMua,
+                     name=request.vars.name,event_name=request.vars.eventName,age=request.vars.age,
+                     phone=request.vars.phone, email=request.vars.email, cach_thanh_toan=request.vars.cachThanhToan,
+                     phuong_thuc_nhan=request.vars.phuongThucNhan, quan_huyen=request.vars.quanhuyen)
+    return dict(id=request.vars.orderId)
+
 def showdetail():
     orderId = request.args[0]
     order_detail = db(db.order_detail.user_id.like(orderId)).select()
