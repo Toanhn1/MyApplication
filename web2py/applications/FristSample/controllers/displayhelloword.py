849431d0f86ceb63575e575dfd1f374e187fd4a5
Toanhn1 <nguyenban61@gmail.com>
2018-08-30 10:11:53 +0700

    Add function update order

--- web2py/applications/FristSample/controllers/displayhelloword.py
+++ web2py/applications/FristSample/controllers/displayhelloword.py
@@ -18,9 +18,23 @@ def addorder():
     return dict(id=request.vars.orderId)
 
 def showdetail():
-    orderId = request.args[0]
-    order_detail = db(db.order_detail.user_id.like(orderId)).select()
+    userId = request.args[0]
+    order_detail = db(db.order_detail.user_id.like(userId)).select()
     return dict(list = order_detail)
+
+def showupdate():
+    orderId = request.args[0]
+    order = db(db.orders.order_id == orderId).select(db.orders.ALL)
+    return dict(showOrder = order)
+
+def doupdate():
+    db(db.orders.order_id == request.vars.orderIdHiden).update(order_id=request.vars.orderId,
+                     user_id=request.vars.userId, ngay_mua=request.vars.ngayMua,
+                     name=request.vars.name,event_name=request.vars.eventName,age=request.vars.age,
+                     phone=request.vars.phone, email=request.vars.email,
+                     cach_thanh_toan=request.vars.cachThanhToan,
+                     phuong_thuc_nhan=request.vars.phuongThucNhan, quan_huyen=request.vars.quanhuyen)
+    return redirect(URL("allorder"))
 
 def deleteorder():
     orderId = request.args[0]
