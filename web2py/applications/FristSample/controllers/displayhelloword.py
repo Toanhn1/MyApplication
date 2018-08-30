da08655291afb7e6254fafb95ad79b3f7d42873c
Toanhn1 <nguyenban61@gmail.com>
2018-08-30 11:13:12 +0700

    Add validate dupble key

--- web2py/applications/FristSample/controllers/displayhelloword.py
+++ web2py/applications/FristSample/controllers/displayhelloword.py
@@ -11,11 +11,18 @@ def formadd():
     return dict()
 
 def addorder():
-    db.orders.insert(order_id=request.vars.orderId, user_id=request.vars.userId, ngay_mua=request.vars.ngayMua,
+    error = 'Duplicate entry '+request.vars.orderId+' for key PRIMARY'
+    order = request.vars.orderId
+    listOrderId = db().select(db.orders.order_id)
+    for row in listOrderId:
+        if order in row.order_id :
+            return redirect(URL("formadd",vars= dict(message=error)))
+        else:
+            db.orders.insert(order_id=request.vars.orderId, user_id=request.vars.userId, ngay_mua=request.vars.ngayMua,
                      name=request.vars.name,event_name=request.vars.eventName,age=request.vars.age,
                      phone=request.vars.phone, email=request.vars.email, cach_thanh_toan=request.vars.cachThanhToan,
                      phuong_thuc_nhan=request.vars.phuongThucNhan, quan_huyen=request.vars.quanhuyen)
-    return dict(id=request.vars.orderId)
+            return dict(id=request.vars.orderId)
 
 def showdetail():
     userId = request.args[0]
@@ -28,18 +35,26 @@ def showupdate():
     return dict(showOrder = order)
 
 def doupdate():
-    db(db.orders.order_id == request.vars.orderIdHiden).update(order_id=request.vars.orderId,
-                     user_id=request.vars.userId, ngay_mua=request.vars.ngayMua,
-                     name=request.vars.name,event_name=request.vars.eventName,age=request.vars.age,
-                     phone=request.vars.phone, email=request.vars.email,
-                     cach_thanh_toan=request.vars.cachThanhToan,
-                     phuong_thuc_nhan=request.vars.phuongThucNhan, quan_huyen=request.vars.quanhuyen)
-    return redirect(URL("allorder"))
+    error = 'Duplicate entry ' + request.vars.orderId + ' for key PRIMARY'
+    order = request.vars.orderId
+    listOrderId = db().select(db.orders.order_id)
+    for row in listOrderId:
+        if order in row.order_id:
+            return redirect(URL("showupdate", vars=dict(message=error)))
+        else:
+            db(db.orders.order_id == request.vars.orderIdHiden).update(order_id=request.vars.orderId,
+                                      user_id=request.vars.userId,ngay_mua=request.vars.ngayMua,
+                                      name=request.vars.name,event_name=request.vars.eventName,
+                                      age=request.vars.age,phone=request.vars.phone,
+                                      email=request.vars.email,cach_thanh_toan=request.vars.cachThanhToan,
+                                      phuong_thuc_nhan=request.vars.phuongThucNhan,
+                                    quan_huyen=request.vars.quanhuyen)
+            return redirect(URL("allorder"))
 
 def deleteorder():
     orderId = request.args[0]
     db(db.orders.order_id == orderId).delete()
     return dict(idDel = orderId)
 
-def showhome():
-    print("")
+def abc():
+    return 'error'
