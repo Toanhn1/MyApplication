c93ab61cf1705f11e3a379a9a071a9bc5599feeb
Toanhn1 <nguyenban61@gmail.com>
2018-09-03 23:11:38 +0700

    Update validate tion

--- web2py/applications/FristSample/controllers/displayhelloword.py
+++ web2py/applications/FristSample/controllers/displayhelloword.py
@@ -6,16 +6,13 @@ def index():
 
 def allorder():
     if not request.vars.page:
-        redirect(URL(vars={'page': 1}))
+        redirect(URL(vars={'page':1}))
     else:
         page = int(request.vars.page)
     start = (page-1)*10
     end = page*10
     listOrder = db(db.orders).select(orderby=db.orders.order_id,limitby=(start,end))
     return dict(list = listOrder)
-
-def abc():
-    return redirect(URL("formadd",vars= dict(message_email='', message='', message_age='',message_date = '')))
 
 def addorder():
     errorKey = 'Duplicate entry '+request.vars.orderId+' for key PRIMARY'
@@ -26,7 +23,7 @@ def addorder():
     listOrderId = db().select(db.orders.order_id)
     for row in listOrderId:
         if order in row.order_id :
-            return redirect(URL("formadd",vars= dict(message=errorKey)))
+            return redirect(URL("formadd",vars= dict(message=errorKey,message_email='', message_age='',message_date = '')))
 
     check_date = re.compile(regex.regrex_day)
     if not check_date.match(request.vars.ngayMua):
@@ -51,7 +48,7 @@ def showdetail():
     return dict(list = order_detail)
 
 def showupdate():
-    orderId = request.args[0]
+    orderId = request.vars.id
     order = db(db.orders.order_id == orderId).select(db.orders.ALL)
     return dict(showOrder = order)
 
@@ -62,16 +59,16 @@ def doupdate():
 
     check_date = re.compile(regex.regrex_day)
     if not check_date.match(request.vars.ngayMua):
-        return redirect(URL("error", vars=dict(message_email='', message='', message_age='',message_date = errorDate)))
+        return redirect(URL("showupdate",vars={'id':request.vars.orderIdHiden,'message_date':errorDate,'message_age':'','message_email':''}))
 
     if not request.vars.age.isnumeric():
-        return redirect(URL("error", vars=dict(message_email='', message='',message_age = errorAge,message_date='')))
+        return redirect(URL("showupdate",vars={'id':request.vars.orderIdHiden,'message_date':'','message_age':errorAge,'message_email':''}))
 
     check_email = re.compile(regex.regrex_email)
     if not check_email.match(request.vars.email):
-        return redirect(URL("error", vars=dict(message_email=errorEmail,message='',message_age='',message_date='')))
+        return redirect(URL("showupdate",vars={'id':request.vars.orderIdHiden,'message_date':'','message_age':'','message_email':errorEmail}))
 
-    db(db.orders.order_id == request.request.vars.orderId).update(user_id=request.vars.userId,ngay_mua=request.vars.ngayMua,
+    db(db.orders.order_id == request.vars.orderIdHiden).update(user_id=request.vars.userId,ngay_mua=request.vars.ngayMua,
                                       name=request.vars.name,event_name=request.vars.eventName,
                                       age=request.vars.age,phone=request.vars.phone,
                                       email=request.vars.email,cach_thanh_toan=request.vars.cachThanhToan,
